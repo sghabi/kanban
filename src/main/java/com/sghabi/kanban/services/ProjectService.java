5878d3662a0b505a101e700c42a942c4e229c731
Sonia Ghabi <sonia.ghabi+git@gmail.com>
2019-07-26 14:58:10 +0800

    Custom exception for Project ID uniqueness

--- src/main/java/com/sghabi/kanban/services/ProjectService.java
+++ src/main/java/com/sghabi/kanban/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.sghabi.kanban.services;
 
 import com.sghabi.kanban.domain.Project;
+import com.sghabi.kanban.exceptions.ProjectIdException;
 import com.sghabi.kanban.repositories.ProjectRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException(
+                    "Project ID '" + project.getProjectIdentifier().toUpperCase() + "' is already used");
+        }
     }
 }
