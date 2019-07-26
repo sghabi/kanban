ae893e205bae5bba30fdcc8c84241a4893725844
Sonia Ghabi <sonia.ghabi+git@gmail.com>
2019-07-26 15:31:43 +0800

    Update project

--- src/main/java/com/sghabi/kanban/web/ProjectController.java
+++ src/main/java/com/sghabi/kanban/web/ProjectController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class ProjectController {
     @Autowired
     private ValidationService validationService;
 
-    @PostMapping("")
+    @PostMapping
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
         ResponseEntity<?> errorMap = validationService.ValidateRequest(result);
         if (errorMap != null)
@@ -42,13 +43,23 @@ public class ProjectController {
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
 
+    @PutMapping
+    public ResponseEntity<?> updateProject(@Valid @RequestBody Project project, BindingResult result) {
+        ResponseEntity<?> errorMap = validationService.ValidateRequest(result);
+        if (errorMap != null)
+            return errorMap;
+
+        projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
     @GetMapping("/{projectIdentifier}")
     public ResponseEntity<?> getProjectByIdentifier(@PathVariable String projectIdentifier) {
         Project project = this.projectService.findProjectByIdentifier(projectIdentifier.toUpperCase());
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 
-    @GetMapping("/")
+    @GetMapping
     public Iterable<Project> getAllProjects() {
         return this.projectService.findAllProjects();
     }
